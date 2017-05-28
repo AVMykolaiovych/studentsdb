a0e28611bd588ab58253e3824c6b1bf1911bf777
avnicolaevich <avnicolaevich@gmail.com>
2017-05-29 00:48:00 +0300

    refactor code

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 
 PORTAL_URL = 'http://localhost:8000'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
