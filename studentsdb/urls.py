01a633ddfc270853b288d32e45da644b10163982
avnicolaevich <avnicolaevich@gmail.com>
2017-06-01 01:03:55 +0300

    urls: add media url

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from students import views
 
@@ -33,4 +35,4 @@ urlpatterns = [
     url(r'^groups/(?P<sid>\d+)/delete/$', views.groups_delete, name='groups_delete'),
 
     url(r'^journal/$', views.journal_list, name='journal'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
