25030494f0dbdd95b9e6ba62147f7c4e8603d1e5
avnicolaevich <avnicolaevich@gmail.com>
2017-06-06 22:17:47 +0300

    templates: add group

--- students/views.py
+++ students/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from .models import Student
+from .models import Student, Group
 
 # Create your views here.
 
@@ -41,21 +41,24 @@ def students_delete(request, sid):
 
 
 def groups_list(request):
-    groups = (
-        {'id': 1,
-         'group_name': 'МтМ-21',
-         'group_head': {'id': 1, 'name': 'Ячменеев Олег'}},
-        {'id': 2,
-         'group_name': 'МтМ-22',
-         'group_head': {'id': 2, 'name': 'Подоба Віталій'}},
-        {'id': 3,
-         'group_name': 'МтМ-23',
-         'group_head': {'id': 3, 'name': 'Бондарчук Богдан'}},
-    )
-    context = {
-        'groups': groups
-    }
-    return render(request, 'students/groups_list.html', context)
+    groups = Group.objects.all()
+
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        groups = paginator.page(1)
+    except EmptyPage:
+        groups = paginator.page(paginator.num_pages)
+
+    return render(request, 'students/groups_list.html', {'groups': groups})
 
 
 def groups_add(request):
